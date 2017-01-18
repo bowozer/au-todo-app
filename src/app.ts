42e22031eb26a7550d89a4c3349b74fd05a7c1b3
bowozer <obakasan.baka@gmail.com>
2017-01-18 22:06:19 +0800

    Todo feature

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,22 @@
+import { Todo } from './todo';
+
 export class App {
   message = 'Hello World!';
+  heading = 'Todos';
+  todoList: Todo[] = [];
+  todoDesc = '';
+
+  addTodo() {
+    if (this.todoDesc) {
+      this.todoList.push(new Todo(this.todoDesc));
+      this.todoDesc = '';
+    }
+  }
+
+  removeTodo(todo) {
+    let index = this.todoList.indexOf(todo);
+    if (index !== -1) {
+      this.todoList.splice(index, 1);
+    }
+  }
 }
